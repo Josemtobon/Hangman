f8e67a972a2387d37c19fbf21e590274e2f40ea9
Jose M. Tobón <josem.tobon@udea.edu.co>
2024-11-07 13:55:33 -0500

    Add clear function

--- hangman.py
+++ hangman.py
@@ -2,8 +2,10 @@
 Starts hangman game.
 """
 
+import os
 import funcs
 
+os.system("clear")
 word = funcs.get_word()
 underscores = funcs.word_underscores(word)
 state = 0
@@ -14,9 +16,11 @@ while underscores != word and state < 6:
     attempt = input("Make a try: ")
     state = funcs.update_state(word, attempt, state)
     underscores = funcs.update_underscores(word, attempt, underscores)
+    os.system('clear')
 
 if state == 6:
     funcs.show_hangman(state)
+    print(word)
     print("\nYou die!!!")
 else:
     funcs.show_hangman(state)
