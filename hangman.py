8063e4107ed7e52a4ce10b9e7c39fd5e03426857
Jose M. Tobón <josem.tobon@udea.edu.co>
2024-11-09 17:39:52 -0500

    Showing tries

--- hangman.py
+++ hangman.py
@@ -9,16 +9,24 @@ os.system("clear")
 word = funcs.get_word()
 underscores = funcs.word_underscores(word)
 state = 0
+your_tries = []
 
 while underscores != word and state < 7:
     while True:
         funcs.show_hangman(state)
         print("        " + underscores)
+        print(f"\nYour tries: {" ".join(your_tries)}")
         attempt = input("\nMake a try: ")
-        os.system('clear')
+
         if len(attempt) == len(word) or len(attempt) == 1:
             break
+
     underscores, state = funcs.update_underscores(word, attempt, underscores, state)
+
+    if len(attempt) == 1:
+        if attempt not in underscores:
+            your_tries.append(attempt)
+
     os.system('clear')
 
 if state == 7:
